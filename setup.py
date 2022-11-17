baadfcef3f44e548753d1f8e101cc1dd22044123
Geoffrey Bourne <gbourne@gmail.com>
2022-11-17 09:58:44 -0500

    Added new functions and docs update.

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.1.0'
+VERSION = '1.1.1'
 PACKAGE_NAME = 'social-post-api'
 AUTHOR = 'Ayrshare'
-AUTHOR_EMAIL = 'contact@ayrshare.com'
+AUTHOR_EMAIL = 'support@ayrshare.com'
 URL = 'https://www.ayrshare.com'
 
 LICENSE = 'Apache License 2.0'
-DESCRIPTION = 'Automate social media posts via Ayrshare\'s API for your company or users. Post to Instagram, Twitter, Facebook, YouTube, LinkedIn, Google My Business, Pinterest, Telegram, and Reddit.'
-KEYWORDS = "Ayrshare, Social Media API, Social Networks, Social Media Management, Socia Posting, Social Automation, Agency Social, Multiple User Posting, Instagram, YouTube API, Twitter API, Facebook API, LinkedIn API, Reddit API, Telegram API, Pinterest API, Google My Business API"
+DESCRIPTION = 'Social Media API: Schedule posts, get analytics, add comments for Instagram, Twitter, Facebook, YouTube, LinkedIn, Google My Business, Pinterest, Telegram, TikTok, and Reddit.'
+KEYWORDS = "Ayrshare, Social Media API, Social Networks, Social Media Management, Social API, Social Posting, Social Analytics, Social Automation, Agency Social, Multiple User Posting, Instagram, YouTube API, Twitter API, Facebook API, LinkedIn API, Reddit API, Telegram API, Pinterest API, Google My Business API, TikTok API"
 HOMEPAGE = "https://www.ayrshare.com"
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
